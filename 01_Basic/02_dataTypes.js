5ade4a9d5f7d0911e1c0166d0a04061051bef50b
priya171299 <76576011+priya171299@users.noreply.github.com>
2025-05-08 10:57:01 +0000

    add data types

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+
+    3 + 7
+
+);  //code readability should be high
+
+console.log(3+3+7);
+
+let name = "legand"
+let age = 18
+let isLogedIn = false
+let state = null;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value  //type of null is object
+// undefined => //type of undefined is undefined
+// symbol => unique
+
+
+console.log(typeof(null)); //output : object
+console.log(typeof(undefined));//output : undefined
+
